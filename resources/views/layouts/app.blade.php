99a8747b8091abc4e4208e938b61967dfb856b51
adiputra <tuts.adiputra@gmail.com>
2020-07-11 17:53:29 +0700

    add footer admin lte

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -157,6 +157,15 @@
         <div class="content-wrapper">
             @yield('content')
         </div>
+
+        <!-- /.content-wrapper -->
+        <footer class="main-footer">
+            <div class="float-right d-none d-sm-block">
+                <b>Version</b> 3.0.5
+            </div>
+            <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights
+            reserved.
+        </footer>
     </div>
 
     <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
